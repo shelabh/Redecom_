deb99ce5d74eb0926c47e4b7534acdfc330715cb
Shelabh <68127067+shelabh@users.noreply.github.com>
2024-06-28 22:51:06 +0530

    removed eslint

--- pages/Landing/Sections/Banner.tsx
+++ pages/Landing/Sections/Banner.tsx
@@ -12,7 +12,7 @@ const Banner = () => {
 					Evolutionary Path of Our Consultancy Agency
 				</div>
 				<div className='w-1/2'>
-					We've established a platform for budding enterpreneurs to master the art of intiating and expanding their businesses successfully.
+					We have established a platform for budding enterpreneurs to master the art of intiating and expanding their businesses successfully.
 				</div>
 				<div>
 					<button className="w-48 flex  flex-row items-center justify-between py-3 px-4 bg-gradient-to-l from-amber-500 to-pink-500 rounded-full">
