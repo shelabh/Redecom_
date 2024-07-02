116a84887802a0061690e95fde725a98184b7f2e
Shelabh <68127067+shelabh@users.noreply.github.com>
2024-07-02 17:04:07 +0530

    added route to navbar

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,14 +1,17 @@
 'use client'
 
+import Link from 'next/link';
 import React from 'react';
 
 
 const Logo = () => {
 	return (
 		<>
+			<Link href="/">
 			<div className=''>
 				<img src="/images/redecom_logo.png" draggable="false" alt="logo" width="120" />
 			</div>
+			</Link>
 		</>
 	)
 }
