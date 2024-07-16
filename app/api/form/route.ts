1815e344737cad672cc91e01769f0f9cdd9848d9
Shelabh <68127067+shelabh@users.noreply.github.com>
2024-07-16 13:03:49 +0530

    changed email sending user

--- app/api/form/route.ts
+++ app/api/form/route.ts
@@ -6,8 +6,8 @@ const transporter = nodemailer.createTransport({
 	port: 465,
 	secure: true,
 	auth: {
-		user: 'zimetsujo@gmail.com',
-		pass: 'fabensrhjksspmzl',
+		user: 'redecom99@gmail.com',
+		pass: 'jmhsdgpiftuhguri',
 	},
 });
 
